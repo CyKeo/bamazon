fbdadbe5cbda327380bae98ebee78e27ff5c7c5c
cyrus <cyruskeo99@gmail.com>
2018-11-29 08:28:57 -0700

    fixed subtaction problem and finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,34 +1,65 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+var colors = require("colors")
 
 var connection = mysql.createConnection({
 	host: "localhost",
 
-	port:3306,
+	port: 3306,
 
 	user: "root",
 
 	password: "password",
-	
+
 	database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
 	if (err) throw err;
 
 	start();
 });
 
-function start(){
-  inquirer
-    .prompt({
-			name: "idSelection",
-			type: "input",
-			message: "What is the id of the item you wish to buy?",
-		},
-		{
-			name: "amount",
-			type: "input",
-			message: "How much of the item would you like to order?"
-		})
+function start() {
+
+	console.log("\n---------------------------------------------------------\n");
+	connection.query("SELECT * FROM products", function (err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			console.log("Item ID: ".bold.gray + res[i].item_id + " Item Name: ".bold.grey + res[i].item_name + " Item Price: ".bold.grey + res[i].price + " Item Quantity: ".bold.grey + res[i].quantity);
+		}
+		console.log("\n");
+		inquirer
+			.prompt([
+				{
+					name: "id",
+					type: "input",
+					message: "What is the id of the item you wish to buy?",
+				},
+				{
+					name: "amount",
+					type: "input",
+					message: "How much of the item would you like to order?",
+				}
+			])
+			.then(function (answer) {
+				var index = res.findIndex(item => item.item_id.toString() === answer.id);
+				connection.query(
+					"UPDATE products SET ? WHERE ?",
+					[
+						{
+							quantity: res[index].quantity - answer.amount
+						},
+						{
+							item_id: answer.id
+						}
+					],
+					function (error) {
+						if (error) throw error;
+						console.log("Item was bought successfully")
+						start()
+					}
+				)
+			})
+	})
 }
--- schema.sql
+++ schema.sql
@@ -7,31 +7,32 @@ USE bamazonDB;
 
 CREATE TABLE products(
 	item_id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(100) NOT NULL,
+	item_name VARCHAR(100) NOT NULL,
 	price INT NOT NULL,
-	quantitiy INT
+	quantity INT,
+	PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Yamsung 50in led", 100, 100) 
+INSERT INTO products (item_name, price, quantity) VALUES ("Yamsung 50in led", 100, 100);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Sphero Gaming console", 300, 150)
+INSERT INTO products (item_name, price, quantity) VALUES ("Sphero Gaming console", 300, 150);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Awake Coffee Brewer" 50, 200)
+INSERT INTO products (item_name, price, quantity) VALUES ("Awake Coffee Brewer" 50, 200);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Boom Ultimate Sound Bar", 90, 50)
+INSERT INTO products (item_name, price, quantity) VALUES ("Boom Ultimate Sound Bar", 90, 50);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Bbuds a Bamazon exlcusive earbud", 80, 1000)
+INSERT INTO products (item_name, price, quantity) VALUES ("Bbuds a Bamazon exlcusive earbud", 80, 1000);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Morgan Crows 100pc kitchen set", 400, 250)
+INSERT INTO products (item_name, price, quantity) VALUES ("Morgan Crows 100pc kitchen set", 400, 250);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Detos nacho cheese chips", 3, 400)
+INSERT INTO products (item_name, price, quantity) VALUES ("Detos nacho cheese chips", 3, 400);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Bepis cola drink", 2, 0)
+INSERT INTO products (item_name, price, quantity) VALUES ("Bepis cola drink", 2, 0);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("Revengers 1 blueray", 20, 30)
+INSERT INTO products (item_name, price, quantity) VALUES ("Revengers 1 blueray", 20, 30);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("SOUP Mens TEE", 10 ,54) 
+INSERT INTO products (item_name, price, quantity) VALUES ("SOUP Mens TEE", 10 ,54);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("SOUP Snapback", 15 ,13) 
+INSERT INTO products (item_name, price, quantity) VALUES ("SOUP Snapback", 15 ,13);
 
-INSERT INTO products (name, price, quantitiy) VALUES ("SOUP BackPack", 35 ,89) 
+INSERT INTO products (item_name, price, quantity) VALUES ("SOUP BackPack", 35 ,89);
